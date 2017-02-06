81a9a09c2a793e42d6b3ca28d8500fc835443fe0
Eric Ewe <eric.ewe@zalora.com>
2017-02-06 13:37:21 +0800

    Add jquery validation

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -17,10 +17,37 @@ $(document).ready(function() {
           $('#page-body').html(source.find('#home-content').html());
         } else {
           $('#page-body').html(response);
+
           if(pageRefInput === 'portfolio.html') {
             $('#portfolio-body').show().animate({top: 0, opacity: 1}, 400);
           } else if(pageRefInput === 'contact.html') {
             $('#contact-body').show().animate({top: 0, opacity: 1}, 400);
+
+            $('#submitForm').on('click', function(e) {
+
+              /** Email validation **/
+              if($('#inputEmail').val().length == 0) {
+                $('#emailFeedback').text('Please enter an email');
+              } else if($('#inputEmail').val().indexOf('@') < 0 || $('#inputEmail').val().indexOf('.com') < 0) {
+                $('#emailFeedback').text('Please enter a valid email');
+              } else {
+                $('#emailFeedback').empty();
+              }
+
+              /** Name validation **/
+              if($('#inputName').val().length == 0) {
+                $('#nameFeedback').text('Please enter a name');
+              } else {
+                $('#nameFeedback').empty();
+              }
+
+              /** Message validation **/
+              if($('#inputMessage').val().length == 0) {
+                $('#msgFeedback').text('Please enter a message');
+              } else {
+                $('#msgFeedback').empty();
+              }
+            });
           }
         }
       },
