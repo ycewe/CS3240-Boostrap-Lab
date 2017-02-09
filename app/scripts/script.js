825092db6a23fc9084745a53a79a0963ddce2422
Eric Ewe <eric.ewe@zalora.com>
2017-02-09 14:30:36 +0800

    Add form validation and submission

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
+  $('#home-body').show().animate({top: 0, opacity: 1}, 400);
+
   $('a').on('click', function(e) {
     e.preventDefault();
     var pageRef = $(this).attr('href');
+
+    if(pageRef === '#') {
+      $('#home-tab').addClass('active');
+      $('#portfolio-tab').removeClass('active');
+      $('#contact-tab').removeClass('active');
+    } else if(pageRef === 'portfolio.html') {
+      $('#home-tab').removeClass('active');
+      $('#portfolio-tab').addClass('active');
+      $('#contact-tab').removeClass('active');
+    } else {
+      $('#home-tab').removeClass('active');
+      $('#portfolio-tab').removeClass('active');
+      $('#contact-tab').addClass('active');
+    }
 
     callPage(pageRef);
   });
@@ -14,7 +30,8 @@ $(document).ready(function() {
       success: function(response) {
         var source = $('' + response + '');
         if(pageRefInput === '#') {
-          $('#page-body').html(source.find('#home-content').html());
+          $('#page-body').html(source.find('#home-content').html())
+          $('#home-body').show().animate({top: 0, opacity: 1}, 400);
         } else {
           $('#page-body').html(response);
 
@@ -24,12 +41,16 @@ $(document).ready(function() {
             $('#contact-body').show().animate({top: 0, opacity: 1}, 400);
 
             $('#submitForm').on('click', function(e) {
+              e.preventDefault();
+              var submit = true;
 
               /** Email validation **/
               if($('#inputEmail').val().length == 0) {
                 $('#emailFeedback').text('Please enter an email');
+                submit = false;
               } else if($('#inputEmail').val().indexOf('@') < 0 || $('#inputEmail').val().indexOf('.com') < 0) {
                 $('#emailFeedback').text('Please enter a valid email');
+                submit = false;
               } else {
                 $('#emailFeedback').empty();
               }
@@ -37,6 +58,7 @@ $(document).ready(function() {
               /** Name validation **/
               if($('#inputName').val().length == 0) {
                 $('#nameFeedback').text('Please enter a name');
+                submit = false;
               } else {
                 $('#nameFeedback').empty();
               }
@@ -44,8 +66,19 @@ $(document).ready(function() {
               /** Message validation **/
               if($('#inputMessage').val().length == 0) {
                 $('#msgFeedback').text('Please enter a message');
+                submit = false;
               } else {
                 $('#msgFeedback').empty();
+              }
+
+              if(submit) {
+                $('.submit-toast').show();
+                var email = 'says3ricrocks@gmail.com';
+                var subject = 'Circle Around';
+                var emailBody = 'Some blah';
+                window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+              } else {
+                $('.submit-toast').hide();
               }
             });
           }
